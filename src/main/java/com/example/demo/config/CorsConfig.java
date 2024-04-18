6cabfdd69d2b611225f65dce526174f1ca2e5a79
Adriian20 <adrianmarquesroqueta@gmail.com>
2024-04-18 18:56:59 +0200

    Añadida config cors

---
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @SuppressWarnings("null")
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000", "https://localhost:8080/pfc") // Agrega las URLs de origen permitidas
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                .allowedHeaders("*") // Encabezados HTTP permitidos
+                .exposedHeaders("Authorization") // Cabezal de respuesta permitido
+                .allowCredentials(true); // Permitir credenciales (cookies)
+    }
+}
+
