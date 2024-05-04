a944520a826f2f33889127e8916d3d885d062806
Adriian20 <adrianmarquesroqueta@gmail.com>
2024-05-04 11:57:43 +0200

    Url nueva añadida a CORS

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000") // Agrega las URLs de origen permitidas
+                .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000", "http://192.168.1.138:3000") // Agrega las URLs de origen permitidas
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
                 .allowedHeaders("*") // Encabezados HTTP permitidos
                 .exposedHeaders("Authorization") // Cabezal de respuesta permitido
