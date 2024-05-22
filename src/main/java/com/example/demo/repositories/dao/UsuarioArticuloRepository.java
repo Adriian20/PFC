99d2e7e846aa9b179e908c1bb70977eb506ba5df
Adriian20 <adrianmarquesroqueta@gmail.com>
2024-05-22 17:55:35 +0200

    Creadas tablas intermedias

---
+++ src/main/java/com/example/demo/repositories/dao/UsuarioArticuloRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repositories.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.repositories.entity.UsuarioArticulo;
+
+public interface UsuarioArticuloRepository extends JpaRepository<UsuarioArticulo, Long> {
+}
---
+++ src/main/java/com/example/demo/repositories/dao/UsuarioVisitaRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repositories.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.repositories.entity.UsuarioVisita;
+
+public interface UsuarioVisitaRepository extends JpaRepository<UsuarioVisita, Long> {
+}
